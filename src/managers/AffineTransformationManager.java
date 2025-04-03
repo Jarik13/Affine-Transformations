82e6c6ee1f17a173b9e838f4701ddd72dfbd15ce
Yaroslav Guz <aroslavguz@gmail.com>
2025-04-04 00:36:34 +0300

    Update generating affine transformation

--- src/managers/AffineTransformationManager.java
+++ src/managers/AffineTransformationManager.java
@@ -9,14 +9,7 @@ public class AffineTransformationManager {
     private List<Point2D.Double> currentPoints = new ArrayList<>();
     private List<Point2D.Double> reflectedPoints = new ArrayList<>();
     private Point2D.Double reflectionPoint;
-    private boolean isAnimating = false;
     private double scaleFactor = 1.0;
-
-    private double[][] scaleMatrix = new double[3][3];
-
-    public AffineTransformationManager() {
-        initializeScaleMatrix();
-    }
 
     public void addPoint(double x, double y) {
         if (currentPoints.size() >= 3) {
@@ -31,6 +24,7 @@ public class AffineTransformationManager {
     public void clearPoints() {
         currentPoints.clear();
         reflectedPoints.clear();
+        reflectionPoint = null;
     }
 
     public List<Point2D.Double> getPoints() {
@@ -47,7 +41,6 @@ public class AffineTransformationManager {
 
     public void setScaleFactor(double scaleFactor) {
         this.scaleFactor = scaleFactor;
-        initializeScaleMatrix();
     }
 
     public void generateReflectedTriangleWithAnimation(final JPanel panel) {
@@ -60,16 +53,39 @@ public class AffineTransformationManager {
 
         clearReflectedPoints();
 
+        double[][] translationToOrigin = {
+                {1, 0, -reflectionPoint.x},
+                {0, 1, -reflectionPoint.y},
+                {0, 0, 1}
+        };
+
+        double[][] reflectionMatrix = {
+                {-1, 0, 0},
+                {0, -1, 0},
+                {0, 0, 1}
+        };
+
+        double[][] scalingMatrix = {
+                {scaleFactor, 0, 0},
+                {0, scaleFactor, 0},
+                {0, 0, 1}
+        };
+
+        double[][] translationBack = {
+                {1, 0, reflectionPoint.x},
+                {0, 1, reflectionPoint.y},
+                {0, 0, 1}
+        };
+
+        double[][] transformMatrix = multiplyMatrices(translationBack,
+                multiplyMatrices(scalingMatrix,
+                        multiplyMatrices(reflectionMatrix, translationToOrigin)));
+
         for (Point2D.Double point : currentPoints) {
-            double reflectedX = 2 * reflectionPoint.x - point.x;
-            double reflectedY = 2 * reflectionPoint.y - point.y;
+            double[] pointVector = {point.x, point.y, 1};
+            double[] transformedPoint = multiplyMatrixAndVector(transformMatrix, pointVector);
 
-            double dx = reflectedX - reflectionPoint.x;
-            double dy = reflectedY - reflectionPoint.y;
-
-            double scaledX = reflectionPoint.x + dx * scaleFactor;
-            double scaledY = reflectionPoint.y + dy * scaleFactor;
-            reflectedPoints.add(new Point2D.Double(scaledX, scaledY));
+            reflectedPoints.add(new Point2D.Double(transformedPoint[0], transformedPoint[1]));
         }
 
         panel.repaint();
@@ -79,10 +95,28 @@ public class AffineTransformationManager {
         return reflectedPoints;
     }
 
-    private void initializeScaleMatrix() {
-        scaleMatrix[0][0] = scaleFactor;
-        scaleMatrix[1][1] = scaleFactor;
-        scaleMatrix[2][2] = 1.0;
+    private double[][] multiplyMatrices(double[][] first, double[][] second) {
+        int rows = first.length, cols = second[0].length, common = second.length;
+        double[][] result = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int k = 0; k < common; k++) {
+                    result[i][j] += first[i][k] * second[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    private double[] multiplyMatrixAndVector(double[][] matrix, double[] vector) {
+        double[] result = new double[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < vector.length; j++) {
+                result[i] += matrix[i][j] * vector[j];
+            }
+        }
+        return result;
     }
 
     public void clearReflectedPoints() {
