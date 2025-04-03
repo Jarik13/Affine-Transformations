80a09bae0ed7c3b220fda1a9e8e66536c5f73a8e
Yaroslav Guz <aroslavguz@gmail.com>
2025-04-03 18:43:22 +0300

    Init AffineTransformationManager

---
+++ src/managers/AffineTransformationManager.java
@@ -0,0 +1,9 @@
+package managers;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AffineTransformationManager {
+    private List<Point2D.Double> currentPoints = new ArrayList<>();
+}
